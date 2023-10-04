dada0b465a0b8de1ebf4af6a08313039de07c2dd
BooMBoTT <b-01@list.ru>
2023-10-04 20:01:58 +0300

    4th_test

--- tester.py
+++ tester.py
@@ -4,6 +4,7 @@ import leetcode.task_m as main
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(main.solution([]), [])
+        self.assertEqual(main.solution([1]), [1])
         self.assertEqual(main.solution([1,2,3]), [6,3,2])
         self.assertEqual(main.solution([5, 6, 1, 7]), [42, 35, 210, 30])
 
