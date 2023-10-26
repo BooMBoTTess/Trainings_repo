b40aa1d358ddb08dffb0260cb555e4e5d8722d38
BooMBoTT <b-01@list.ru>
2023-10-26 16:09:44 +0300

    t2

---
+++ yandex_algorithm/t2.py
@@ -0,0 +1,21 @@
+def Solution(s):
+    index = s.split('\n')
+    srting1 = list(map(int, index[0].split(' ')))
+    n = srting1[0]
+    b = srting1[1]
+    arr = list(map(int, index[1].split(' ')))
+    dp = [0]
+
+    for now in range(1, n):
+        tmp = [0]
+        for prev in range(now):
+            if (arr[prev] <= arr[now]) and ((arr[prev] + b) >= arr[now]):
+                tmp.append(dp[prev]+1)
+        dp.append(max(tmp))
+
+    return max(dp)+1, dp
+
+
+if __name__ == '__main__':
+    f = open('input.txt', 'r')
+    print(Solution(f.read().rstrip('\n')))
