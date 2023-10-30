8b6c39864fb8614465f93ce24245d3093b0b7cda
BooMBoTT <b-01@list.ru>
2023-10-30 21:27:13 +0300

    piupiu

--- 40yand/razm2.py
+++ 40yand/razm2.py
@@ -1,12 +1,8 @@
-def mygcd(m, n):
-    while (m != 0) and (n != 0):
-        if m > n:
-            m = m % n
-        else:
-            n = n % m
-    if n == 0:
-        return m
-    return n
+def mygcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
 
 def Solution(s):
     a, b, c, d = list(map(int, s.split(' ')))
@@ -14,13 +10,7 @@ def Solution(s):
     n = a * d + c * b
     i = max(m, n)
     nod = 1
-    if (m % n != 0) and (n % m != 0):
-        nod = mygcd(m, n)
-    else:
-        for i in range(i, 1, -1):
-            if m % i == 0:
-                nod = i
-                break
+    nod = mygcd(m, n)
     m = m//nod
     n = n//nod
 
