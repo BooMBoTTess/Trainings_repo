d096237fa93b8a8653289a86b42e6d47f8a56f3e
BooMBoTT <b-01@list.ru>
2023-10-30 19:52:49 +0300

    t1razm commit

---
+++ 40yand/razm1.py
@@ -0,0 +1,29 @@
+def sol(tmp):
+    minimum = min(tmp)
+    for el in range(len(tmp)):
+        if tmp[el] != minimum:
+            return tmp[el]
+    return 'NOT FOUND'
+def Solution(s):
+    s = s.split('\n')
+    input_str = list(map(int, s[1].split(' ')))
+    arr = s[2:]
+    print(arr, input_str)
+    tmp = []
+    for elem in arr:
+        elem = list(map(int, elem.split(' ')))
+        tmp = input_str[elem[0] : elem[1]+1]
+
+        ans = -1
+        print(sol(tmp))
+
+
+
+
+
+    return arr, input_str
+
+
+if __name__ == '__main__':
+    f = open('input.txt', 'r')
+    Solution(f.read().rstrip('\n'))
