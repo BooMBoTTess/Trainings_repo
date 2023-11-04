ff838ef939250169f412603b37d1358d3570e857
BooMBoTTess <piu@mail.ru>
2023-11-04 12:23:16 +0300

    podshet

--- 40yand/1 lesson/task_podshet.py
+++ 40yand/1 lesson/task_podshet.py
@@ -1,17 +1,37 @@
+def make_bucker(arr, phase: int):
+    print('**********')
+    print('Phase', phase)
+    bucket = [[] for i in range(10)]
+    for elem in arr:
+        bucket[int(elem[0 - phase])].append(elem)
+    #print(bucket)
+    return bucket
+
+
 def Solution(arr):
+    print('Initial array:')
+    print(' '.join(arr))
+    for phase in range(1, len(arr[0])+1):
+        bucket = make_bucker(arr, phase)
+        arr = []
+        counter = 0
+        for elem in bucket:
+            if elem != []:
+                print(f"Bucket {counter}: {' '.join(elem)}")
+                for i in elem:
+                    arr.append(i)
+            else:
+                print(f"Bucket {counter}: Empty")
+            counter += 1
+    print('**********')
+    print('Sorted array:')
+    print(', '.join(arr))
 
 
 
 if __name__ == '__main__':
     f = open('input.txt', 'r')
     n = f.readline()
-    if int(n) == 0:
-        arr1 = []
-    else:
-        arr1 = list(map(int, f.readline().split(' ')))
-    m = f.readline()
-    if int(m) == 0:
-        arr2 = []
-    else:
-        arr2 = list(map(int, f.readline().split(' ')))
-    print(' '.join(list(map(str, merge(arr1, arr2)))))
+    arr = f.read().split('\n')
+    Solution(arr)
+    #print(' '.join(list(map(str, Solution(arr)))))
