d28d373ce110da0f3deac3525f441d70ca40e05e
BooMBoTTess <piu@mail.ru>
2023-11-04 11:55:09 +0300

    dsa

--- 40yand/1 lesson/taskmergesort.py
+++ 40yand/1 lesson/taskmergesort.py
@@ -37,5 +37,13 @@ def Solution(arr):
 if __name__ == '__main__':
     f = open('input.txt', 'r')
     n = f.readline()
-    inp = list(map(int, f.read().rstrip('\n').split(' ')))
-    print(' '.join(list(map(str, Solution(inp)))))
+    if int(n) == 0:
+        arr1 = []
+    else:
+        arr1 = list(map(int, f.readline().split(' ')))
+    m = f.readline()
+    if int(m) == 0:
+        arr2 = []
+    else:
+        arr2 = list(map(int, f.readline().split(' ')))
+    print(' '.join(list(map(str, merge(arr1, arr2)))))
