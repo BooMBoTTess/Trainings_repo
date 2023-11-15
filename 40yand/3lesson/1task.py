798239ffc5da36e60138d722e00af79a0b2bbcb7
Artem Maliy <78168852+BooMBoTTess@users.noreply.github.com>
2023-11-15 20:59:51 +0300

    Update 1task.py

--- 40yand/3lesson/1task.py
+++ 40yand/3lesson/1task.py
@@ -43,16 +43,19 @@ def Solution(n, S1, S2, matrix):
 if __name__ == '__main__':
     with open('input.txt','r') as f:
         n, S1, S2 = list(map(int, f.readline().rstrip('\n').split(' ')))
-        matrix = f.read().rstrip('\n').split('\n')
-        matrix2 = []
-        S1 -= 1
-        S2 -= 1
-        for row in matrix:
-            matrix2.append(list(map(int, row.split(' '))))
-        matrix = matrix2
-    m_sm = smegn(n,matrix)
-    dist_sol = Solution(n, S1, S2, m_sm)[S2]
-    if dist_sol == 100000:
-        print(-1)
+        if S1 == S2:
+            print(0)
+        else:
+            matrix = f.read().rstrip('\n').split('\n')
+            matrix2 = []
+            S1 -= 1
+            S2 -= 1
+            for row in matrix:
+                matrix2.append(list(map(int, row.split(' '))))
+            matrix = matrix2
+            m_sm = smegn(n,matrix)
+            dist_sol = Solution(n, S1, S2, m_sm)[S2]
+            if dist_sol == 100000:
+                print(-1)
     else:
         print(dist_sol)
