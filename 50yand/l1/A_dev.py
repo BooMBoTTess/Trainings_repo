f3bcc9f5d7099c558758a8ea1c76d8afc1bd18ab
BooMBoTTess <piu@mail.ru>
2024-03-10 17:35:56 +0300

    pipu

---
+++ 50yand/l1/A_dev.py
@@ -0,0 +1,9 @@
+def Solution(P, V, Q, M):
+    pass
+
+
+if __name__ == '__main__':
+    with open('input.txt', 'r') as f:
+        P, V = f.readline().strip('\n').split(' ')
+        Q, M = f.readline().strip('\n').split(' ')
+        print(P, V, Q, M)
