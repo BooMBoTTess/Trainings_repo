8c3aeb6c34ebcc00458436c701d853f8dde991fd
BooMBoTTess <piu@mail.ru>
2024-03-12 20:02:10 +0300

    помогите

--- 50yand/l1/B_dev.py
+++ 50yand/l1/B_dev.py
@@ -1,9 +1,26 @@
 def Solution(match_prev, match, home):
     total_score = (match_prev[0] + match[0], match_prev[1] + match[1])
     need = total_score[1] - total_score[0]
-    print(need)
+    if need < 0:
+        return 0
+    if need == 0:
+        if home == 2:
+            if match_prev[0] <= match[1]:
+                return 1
+            else:
+                return 0
+        else:
+            if match_prev[1] >= match[0]:
+                return 1
+            else:
+                return 0
+
     if home == 2:
-        return need + 1
+        #2я игра дома
+        if match_prev[0] >= match[1]:
+            return need
+        else:
+            return need + 1
     else:
         if match_prev[1] >= need:
             return need + 1
